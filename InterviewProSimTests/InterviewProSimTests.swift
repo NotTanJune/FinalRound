f920efc6a1bd3f8597d41bebecf2287f877577cb
Tanmay Nargas <tanmaynargas291@gmail.com>
2025-11-04 17:24:29 +0800

    Initial Commit

---
+++ InterviewProSimTests/InterviewProSimTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewProSimTests.swift
+//  InterviewProSimTests
+//
+//  Created by Tanmay Nargas on 04/11/25.
+//
+
+import Testing
+@testable import InterviewProSim
+
+struct InterviewProSimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
